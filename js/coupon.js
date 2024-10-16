0448203ec23c6f4cb437f239008dc4ac893c354a
Montaser <montasergohar@gmail.com>
2024-10-16 22:53:36 +0300

    Upload task

---
+++ js/coupon.js
@@ -0,0 +1,36 @@
+const sidebar = document.getElementById('sidebar');
+const toggleBtn = document.getElementById('toggle-btn');
+
+toggleBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('collapsed'); // إخفاء أو إظهار الشريط الجانبي
+
+  if (sidebar.classList.contains('collapsed')) {
+    toggleBtn.classList.remove('rotate'); // إزالة التدوير
+  } else {
+    toggleBtn.classList.add('rotate'); // تدوير السهم 90 درجة
+  }
+});
+
+document.querySelector('.dropdown-toggle').addEventListener('click', function () {
+  const dropdownMenu = document.getElementById('dropdown-menu');
+  dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+});
+
+const options = document.querySelectorAll('.dropdown-content div');
+options.forEach(option => {
+  option.addEventListener('click', function () {
+    const selectedUser = this.getAttribute('data-value');
+    document.getElementById('selected-user').innerText = selectedUser;
+    document.querySelector('.dropdown-content').style.display = 'none';
+  });
+});
+
+// Close dropdown if clicked outside
+window.onclick = function (event) {
+  if (!event.target.matches('.dropdown-toggle')) {
+    const dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      dropdowns[i].style.display = "none";
+    }
+  }
+}
